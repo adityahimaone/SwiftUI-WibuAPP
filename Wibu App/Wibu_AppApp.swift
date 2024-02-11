325b89ea3970876e22e5993e9eee98730067b8c5
adityahimaone <adityahimaone@gmail.com>
2024-02-11 12:41:26 +0700

    Initial Commit

---
+++ Wibu App/Wibu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wibu_AppApp.swift
+//  Wibu App
+//
+//  Created by Aditya Himawan on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Wibu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
